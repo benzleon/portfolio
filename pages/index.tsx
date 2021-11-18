86cf66f3a2e7c027dd112c8097e4df75bd8f7ceb
Leon Benz <49094433+benzleon@users.noreply.github.com>
2021-11-18 16:09:55 +0100

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ const Home = () => {
   return (
     <>
       <Head>
+        <meta name="description" content="Ich bin Leon Benz ein Programmierer / Frontend Entwickler. Dies ist mein Portfolio" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="leon, benz, programmierer, entwickler, fullstack, react, programmer" />
         <title>Leon Benz | Frontendentwickler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
